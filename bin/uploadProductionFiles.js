f46d11672d09109795b08e8da01426763b1d9d2a
eaTong <eatongchou@gmail.com>
2019-11-18 14:28:40 +0800

    add note support

--- bin/uploadProductionFiles.js
+++ bin/uploadProductionFiles.js
@@ -2,14 +2,22 @@ const client = require('scp2');
 const fs = require('fs');
 client.addListener('transfer', (a, b, c) => {
   console.log(a, b, c);
-})
+});
+
+fs.existsSync('C:\\certificate');
 
 client.scp('build/', {
-  host: '118.24.75.69',
-  username: 'ubuntu',
-  privateKey: require("fs").readFileSync('/Users/liuyajun/certifcate/eaTong'),
-  passphrase: '',
-  path: '/home/ubuntu/build'
-}, function (err, ...args) {
-  console.log(err, ...args);
-});
+    host: '118.24.75.69',
+    username: 'ubuntu',
+    privateKey: fs.readFileSync(fs.existsSync('C:\\certificate') ? 'C:\\certificate\\eaTong' : '/Users/liuyajun/certifcate/eaTong'),
+    passphrase:
+      '',
+    path:
+      '/home/ubuntu/build'
+  },
+
+  function (err, ...args) {
+    console.log(err, ...args);
+  }
+)
+;
