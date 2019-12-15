71654a3a79f73d6e3ce6efd85fa2c80ea4331fb0
eaTong <eatongchou@gmail.com>
2019-12-15 13:10:45 +0800

    add source

--- server/models/Note.js
+++ server/models/Note.js
@@ -9,7 +9,8 @@ const sequelize = require('../framework/database');
 const Note = sequelize.define('note', {
   title: {type: Sequelize.STRING},
   content: {type: Sequelize.TEXT},
-  reference: {type: Sequelize.TEXT},
+  reference: {type: Sequelize.TEXT,comments:'引用'},
+  source: {type: Sequelize.TEXT,comments:'来源',},
   images:{type:Sequelize.JSON},
   enable: Sequelize.BOOLEAN,
 });
