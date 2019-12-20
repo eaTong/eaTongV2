2058cc3efae8e0aa0fbf2bae1841fa44fd11ad10
eaTong <eatongchou@gmail.com>
2019-12-20 11:58:23 +0800

    optimise index

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,23 @@
  * Description:
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Page from "../website/components/Page";
-import { inject, observer } from "mobx-react";
+import {inject, observer} from "mobx-react";
 import ajax from "../website/util/ajax";
-import { Link } from '../page-routes';
-import { formatTime } from "../website/util/utils";
+import {Link} from '../page-routes';
+import {formatTime} from "../website/util/utils";
 
 @inject('home', 'app') @observer
 class IndexPage extends Component {
   static async init(ctx) {
-    const { list, total } = await ajax({ ctx, method: 'get', url: '/api/pub/blog/get' });
-    const notes = await ajax({ ctx, method: 'get', url: '/api/pub/note/get' });
-    return { home: { blog: list, total, notes: notes.list } };
+    const {list, total} = await ajax({ctx, method: 'get', url: '/api/pub/blog/get'});
+    const notes = await ajax({ctx, method: 'get', url: '/api/pub/note/get'});
+    return {home: {blog: list, total, notes: notes.list}};
   }
 
   render() {
-    const { home } = this.props;
+    const {home} = this.props;
     return (
       <div className="index-page">
         <section className="hero  is-primary">
@@ -58,17 +58,17 @@ class IndexPage extends Component {
               </Link>
             ))}
           </div>
-          <div className="notes-list">
+          <div className="note-list-container">
             <h4 className={'title'}>我的笔记</h4>
 
-            <div className="columns is-multiline ">
-
+            <div className="note-list">
               {home.notes.map(note => (
-                <div className={`column  is-${Math.ceil(note.content.length / 80) + (note.reference ? 2 : 0)} `}>
-                  <div className="box content">
+                <div
+                  key={note.id}
+                  className={`note-item box span-${Math.max(Math.ceil(note.content.length / 150) + 1 + (note.reference ? 1 : 0))}`}>
+                  <div className="content">
                     {note.reference && (
                       <article className='message'>
-
                         <div className='message-body'>
                           <p>{note.reference}</p>
 
