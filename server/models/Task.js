6c683a3fc50b30c3484711659bcc3f7ee0fa274b
eaTong <eatongchou@gmail.com>
2021-01-13 12:02:09 +0800

    修复地址bug

--- server/models/Task.js
+++ server/models/Task.js
@@ -9,6 +9,9 @@ const sequelize = require('../framework/database');
 
 const Task = sequelize.define('task', {
   name: {type: Sequelize.STRING},
+  deadline: {type: Sequelize.DATE},
+  hasDone: {type: Sequelize.BOOLEAN},
+  description: {type: Sequelize.STRING},
   enable: Sequelize.BOOLEAN,
 });
 
